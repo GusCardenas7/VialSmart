04a74906b3e3f0c0ca09bc0848e86af875ca1da2
Gus Cardenas <gustavo.cardenas5755@alumnos.udg.mx>
2024-08-13 00:49:27 -0600

    Integré todas las páginas en la branch master

--- funciones/menu.php
+++ funciones/menu.php
@@ -5,7 +5,7 @@
         <li> <b><a href="../Seguridad Vial/IndexSV.php">Inicio</a></b></li>
         <li> <b><a href="/">Lecciones</a></b></li>
         <li> <b><a href="../Seguridad Vial/videos.php">Videos</a></b></li>
-        <li> <b><a href="/">Juegos</a></b></li>
+        <li> <b><a href="../Seguridad Vial/Juego1.php">Juegos</a></b></li>
         <li> <b><a href="../Seguridad Vial/login.php">Registrarse/Iniciar Sesi&#243;n</a></b></li>
     </ul>
 </div>
